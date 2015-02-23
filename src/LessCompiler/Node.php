e0cef569c2d39face6566d9d44866edf2de7a80d
bound1ess <bound1ess.dev@gmail.com>
2015-02-23 20:44:43 +0600

    Cover Node.

--- src/LessCompiler/Node.php
+++ src/LessCompiler/Node.php
@@ -2,6 +2,35 @@
 
 class Node {
 
-    // ...
+    /**
+     * @var mixed
+     */
+    protected $value;
+
+    /**
+     * @param mixed $value
+     * @return Node
+     */
+    public function __construct($value = null)
+    {
+        $this->setValue($value);
+    }
+
+    /**
+     * @param mixed $value
+     * @return void
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
 
 }
--- tests/LessCompiler/NodeTest.php
+++ tests/LessCompiler/NodeTest.php
@@ -7,6 +7,16 @@ class NodeTest extends \TestCase {
      */
     protected $sut = "LessCompiler\\Node";
 
-    // ...
+    /**
+     * @test
+     */
+    public function it_stores_a_value()
+    {
+        expect($this->sut->getValue())->to_be_null();
+
+        $this->sut->setValue("foobar");
+
+        expect($this->sut->getValue())->to_be_equal_to("foobar");
+    }
 
 }
