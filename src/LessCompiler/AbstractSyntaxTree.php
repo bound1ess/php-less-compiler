f260910f5b1510af308fd129d23b5ec637258f65
bound1ess <bound1ess.dev@gmail.com>
2015-02-23 21:21:34 +0600

    Work on AbstractSyntaxTree.

--- src/LessCompiler/AbstractSyntaxTree.php
+++ src/LessCompiler/AbstractSyntaxTree.php
@@ -2,6 +2,46 @@
 
 class AbstractSyntaxTree {
 
-    // ...
+    /**
+     * @var array
+     */
+    protected $nodes = [];
+
+    /**
+     * @param array $nodes
+     * @return AbstractSyntaxTree
+     */
+    public function __construct(array $nodes = [])
+    {
+        $this->addNodes($nodes);
+    }
+
+    /**
+     * @param array $nodes
+     * @return void
+     */
+    public function addNodes(array $nodes)
+    {
+        foreach ($nodes as $node) {
+            $this->addNode($node);
+        }
+    }
+
+    /**
+     * @param Node $node
+     * @return void
+     */
+    public function addNode(Node $node)
+    {
+        $this->nodes[] = $node;
+    }
+
+    /**
+     * @return array
+     */
+    public function getNodes()
+    {
+        return $this->nodes;
+    }
 
 }
--- tests/LessCompiler/AbstractSyntaxTreeTest.php
+++ tests/LessCompiler/AbstractSyntaxTreeTest.php
@@ -7,6 +7,28 @@ class AbstractSyntaxTreeTest extends \TestCase {
      */
     protected $sut = "LessCompiler\\AbstractSyntaxTree";
 
-    // ...
+    /**
+     * @test
+     */
+    public function it_manages_nodes()
+    {
+        expect($this->sut->getNodes())->to_have_length(0);
+
+        $this->sut->addNodes([$this->createNodeMock(), $this->createNodeMock()]);
+
+        expect($this->sut->getNodes())->to_have_length(2);
+
+        $this->sut->addNode($this->createNodeMock());
+
+        expect($this->sut->getNodes())->to_have_length(3);
+    }
+
+    /**
+     * @return object
+     */
+    protected function createNodeMock()
+    {
+        return \Mockery::mock("LessCompiler\\Node");
+    }
 
 }
