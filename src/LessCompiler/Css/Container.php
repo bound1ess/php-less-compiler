1ab46f274ea27d86a2d60d5f7c7e20f1d89381e8
bound1ess <bound1ess.dev@gmail.com>
2015-02-24 22:51:41 +0600

    Add Container.

---
+++ src/LessCompiler/Css/Container.php
@@ -0,0 +1,45 @@
+<?php namespace LessCompiler\Css;
+
+/**
+ * A container that contains:
+ *    1) a query (selector) string
+ *    2) a set of properties
+ */
+class Container extends \LessCompiler\Node {
+
+    /**
+     * @param \LessCompiler\Css\Query $query
+     * @param array $properties
+     * @return Container
+     */
+    public function __construct(Query $query, array $properties = [])
+    {
+        parent::__construct([
+            "query"      => $query,
+            "properties" => [],
+        ]);
+
+        $this->addProperties($properties);
+    }
+
+    /**
+     * @param array $properties
+     * @return void
+     */
+    public function addProperties(array $properties)
+    {
+        foreach ($properties as $property) {
+            $this->addProperty($property);
+        }
+    }
+
+    /**
+     * @param \LessCompiler\Css\Property $property
+     * @return void
+     */
+    public function addProperty(Property $property)
+    {
+        $this->value["properties"][] = $property;
+    }
+
+}
