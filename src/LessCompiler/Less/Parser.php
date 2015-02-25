024b8cd4155b707be826422d9267b1d36ac8fa48
bound1ess <bound1ess.dev@gmail.com>
2015-02-25 20:48:23 +0600

    Work on Parser.

--- src/LessCompiler/Less/Parser.php
+++ src/LessCompiler/Less/Parser.php
@@ -6,12 +6,52 @@
 class Parser {
 
     /**
+     * @var \SplQueue
+     */
+    protected $queue;
+
+    /**
      * @param string $code
      * @return \LessCompiler\Less\LessTree
      */
     public function parse($code)
     {
-        // @todo
+        $this->intoQueue($code);
+
+        $tree = new LessTree;
+
+        while ( ! $this->queue->isEmpty()) {
+            // @do whatever
+        }
+
+        return $tree;
+    }
+
+    /**
+     * @param string $code
+     * @return void
+     */
+    protected function intoQueue($code)
+    {
+        $this->queue = new \SplQueue;
+
+        foreach (array_filter(array_map("trim", explode(PHP_EOL, $code))) as $line) {
+            $this->queue->enqueue($line);
+        }
+    }
+
+    /**
+     * @throws \LessCompiler\Less\Exceptions\ParseException
+     * @return string
+     */
+    protected function readLine()
+    {
+        try {
+            return $this->queue->dequeue();
+        } catch(Exceptions\ParseException $exception) {
+            // @todo
+            throw new Exceptions\ParseException($exception->getMessage());
+        }
     }
 
 }
