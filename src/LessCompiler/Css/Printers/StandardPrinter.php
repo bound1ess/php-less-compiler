30327de89d99a023f7708d8b4807543e0e5eaa69
bound1ess <bound1ess.dev@gmail.com>
2015-02-25 14:02:53 +0600

    Add Printer.

---
+++ src/LessCompiler/Css/Printers/StandardPrinter.php
@@ -0,0 +1,16 @@
+<?php namespace LessCompiler\Css\Printers;
+
+/**
+ * {@inheritdoc}
+ */
+class StandardPrinter extends Printer {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function printTree(\LessCompiler\Css\CssTree $tree)
+    {
+        // @todo
+    }
+
+}
