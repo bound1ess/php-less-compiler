1a104d92c633e30ce8b73b60de172a87543c6ebe
bound1ess <bound1ess.dev@gmail.com>
2015-02-28 15:30:55 +0600

    Add TreeDumper.

---
+++ src/LessCompiler/TreeDumper.php
@@ -0,0 +1,17 @@
+<?php namespace LessCompiler;
+
+/**
+ * An AST dumper.
+ */
+class TreeDumper {
+
+    /**
+     * @param \LessCompiler\AbstractSyntaxTree $tree
+     * @return string
+     */
+    public function dumpTree(AbstractSyntaxTree $tree)
+    {
+        return "Hello, world!";
+    }
+
+}
