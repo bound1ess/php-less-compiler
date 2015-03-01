06240a2da174612aec9660bee9eb8289b138e277
bound1ess <bound1ess.dev@gmail.com>
2015-03-01 19:37:59 +0600

    ScopeManager?

--- src/LessCompiler/Compiler/ScopeManager.php
+++ src/LessCompiler/Compiler/ScopeManager.php
@@ -5,6 +5,32 @@
  */
 class ScopeManager {
 
-    // ...
+    /**
+     * @var array
+     */
+    protected $scopes = [];
 
+    /**
+     * @return ScopeManager
+     */
+    public function __construct()
+    {
+        // Add "global" scope.
+        $this->scopes["global"] = new Scope;
+    }
+
+    /**
+     * @param string $id
+     * @param Scope|null $parentScope
+     * @return Scope
+     */
+    public function getScope($id, Scope $parentScope = null)
+    {
+        if ( ! array_key_exists($id, $this->scopes)) {
+            $this->scopes[$id] = new Scope;
+            $this->scopes[$id]->setParentScope($parentScope);
+        }
+
+        return $this->scopes[$id];
+    }
 }
