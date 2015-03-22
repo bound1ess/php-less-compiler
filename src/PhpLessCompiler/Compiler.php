cd142be8efcf6f223b9828a6522ea6845c581ac4
bound1ess <bound1ess.dev@gmail.com>
2015-03-22 16:04:52 +0600

    LessCompiler => PhpLessCompiler

---
+++ src/PhpLessCompiler/Compiler.php
@@ -0,0 +1,14 @@
+<?php namespace PhpLessCompiler;
+
+class Compiler {
+
+    /**
+     * @param string $less
+     * @return string
+     */
+    public function compile($less)
+    {
+        // @todo
+        return $less;
+    }
+}
