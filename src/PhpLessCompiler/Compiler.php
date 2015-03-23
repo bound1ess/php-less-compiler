ea52693971cb20c833b46fe8e31b5a17a654eaa8
bound1ess <bound1ess.dev@gmail.com>
2015-03-23 21:58:52 +0600

    Plug in all components.

--- src/PhpLessCompiler/Compiler.php
+++ src/PhpLessCompiler/Compiler.php
@@ -1,6 +1,43 @@
 <?php namespace PhpLessCompiler;
 
+use PhpLessCompiler\Parser\Parser,
+    PhpLessCompiler\Parser\Statements\DeclarationStatement,
+    PhpLessCompiler\Parser\Statements\VarStatement,
+    PhpLessCompiler\Parser\Statements\ImportStatement;
+
+use PhpLessCompiler\Compiler\ScopeManager;
+
+use PhpLessCompiler\CssPrinter\Printer,
+    PhpLessCompiler\CssPrinter\Printers\Printer as PrinterContract;
+
 class Compiler {
+
+    /**
+     * @var Parser
+     */
+    protected $parser;
+
+    /**
+     * @var ScopeManager
+     */
+    protected $scopeManager;
+
+    /**
+     * @var PrinterContract
+     */
+    protected $printer;
+
+    /**
+     * @return Compiler
+     */
+    public function __construct()
+    {
+        $this->parser = new Parser;
+
+        $this->scopeManager = new ScopeManager;
+
+        $this->printer = Printer::make('default');
+    }
 
     /**
      * @param string $less
